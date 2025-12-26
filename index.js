c9c87fd95bd100f531303ebe4c7400c8f182d1d8
klung <klung.sk@hotmail.com>
2025-12-26 11:55:13 +0700

    update version2

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const express = require('express')
 const app = express()
 const PORT = 4000
 
-app.listen(PORT , () => {
-  console.log(`API Listening on PORT ${PORT}`)
-})
-
 app.get('/',(req ,res) => {
   res.send('This is my API Running...')
 })
@@ -13,6 +9,10 @@ app.get('/',(req ,res) => {
 app.get('/about',(req ,res) => {
   res.send('This is my about route')
 })
+
+app.listen(PORT, () => {
+  console.log(`API Listening on PORT ${PORT}`);
+});
 
 module.exports = app
 
